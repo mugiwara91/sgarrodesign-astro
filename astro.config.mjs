0a3038b99e35ec1357e6c84bb34ea6ec7418e5d3
Simone <simone@MacBook-Pro-di-Simone.local>
2026-09-10 18:09:34 +0200

    le tue modifiche

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,12 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
+import partytown from '@astrojs/partytown';
+
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+
+  integrations: [partytown()],
 });
